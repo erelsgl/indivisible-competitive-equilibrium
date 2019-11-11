f4362d9a03c91212bfd7134f81c2b552847eca02
Erel Segal-Halevi cron <erelsgl@gmail.com>
2019-11-11 18:55:56 +0200

    order after lecture 3

--- tests/test_linprog.py
+++ tests/test_linprog.py
@@ -9,6 +9,9 @@ b = [6, 4]
 x0_bounds = (None, None)
 x1_bounds = (-3, None)
 
+# Minimize: c * x
+# Subject to: A_ub * x <= b_ub
+# A_eq * x == b_eq
 res = linprog(c, A_ub=A, b_ub=b, bounds=[x0_bounds, x1_bounds])
 print(res)
 print()
